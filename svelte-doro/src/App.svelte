fd84b6170ae25fb2530333eff6a464b2820b6a6d
Jethro Lising <jethro.lorenzo.lising@gmail.com>
2019-12-11 05:56:18 -0500

    Using setInterval now

--- svelte-doro/src/App.svelte
+++ svelte-doro/src/App.svelte
@@ -1,54 +1,46 @@
 <script>
   import { onDestroy } from "svelte";
-  let elapsed = 0;
-  let timer = 5000;
-  let last_time = window.performance.now();
-  let frame;
+
+  let seconds = 600;
   let started = false;
-  let selected;
-  let answer = "";
 
-  let presets = [
-    { id: 1, text: `Work`, duration: 5000 },
-    { id: 2, text: `Rest`, duration: 5000 }
-  ];
+  $: display = sec2time(seconds);
 
-  (function update() {
-    frame = requestAnimationFrame(update);
-    const time = window.performance.now();
-    // console.log(last_time, time);
-    // console.log(duration - elapsed);
+  function toPretty(s) {
+    var a = new Date(s);
+    return a
+      .toISOString()
+      .substr(11, 8)
+      .toString();
+  }
+  function sec2time(timeInSeconds) {
+    var pad = function(num, size) {
+        return ("000" + num).slice(size * -1);
+      },
+      time = parseFloat(timeInSeconds).toFixed(3),
+      hours = Math.floor(time / 60 / 60),
+      minutes = Math.floor(time / 60) % 60,
+      seconds = Math.floor(time - minutes * 60),
+      milliseconds = time.slice(-3);
 
-    if (timer === 0) {
-      started = false;
+    return pad(hours, 2) + ":" + pad(minutes, 2) + ":" + pad(seconds, 2);
+  }
+
+  const interval = setInterval(() => {
+    if (started) {
+      seconds -= 1;
+      display = toPretty(seconds);
+      display = display;
     }
+  }, 1000);
 
-    if (timer !== 0 && started) {
-      console.log("ping");
-      timer -= Math.min(time - last_time, timer - elapsed);
-    }
-
-    console.log(timer);
-    last_time = time;
-  })();
-  onDestroy(() => {
-    cancelAnimationFrame(frame);
-  });
+  onDestroy(() => clearInterval(interval));
 </script>
 
-<label>
-  elapsed time:
-  <progress value={timer} />
-</label>
-
-<div>{(timer / 1000).toFixed(1)}s</div>
-<label>started: {started}</label>
-
-<select bind:value={selected} on:change={() => (answer = '')}>
-  {#each presets as preset}
-    <option value={preset}>{preset.text}</option>
-  {/each}
-</select>
-
-<button on:click={() => (started = !started)}>Start</button>
-<button on:click={() => (timer = 5000)}>Reset</button>
+<h1>Hey!</h1>
+<p>{seconds}</p>
+<p>{display}</p>
+<button on:click={() => (started = !started)}>
+  {started ? 'Stop' : 'Start'}
+</button>
+<button on:click={() => (seconds = 0)}>Reset</button>
